51d1187e2cb369bd7145c3372bb73e07e427edf9
TungTop <stefanitvn@gmail.com>
2021-01-18 16:54:40 -0600

    lesson4

---
+++ common/common.js
@@ -0,0 +1,31 @@
+let validateArr = [
+    {
+        href : "https://validator.w3.org/check?uri=",
+        img : "w3c-html.png",
+        alt: "html validator"
+    },
+
+    {
+        href : "http://jigsaw.w3.org/css-validator/validator?uri=",
+        img : "w3c-css.png",
+        alt: "CSS validator"
+    },
+
+    {
+        href : "http://jigsaw.w3.org/css-validator/validator?uri=",
+        img : "w3c-css.png",
+        alt: "CSS validator"
+    },
+]
+
+function renderValidate(limit = 2) {
+    let currentUrl = window.location.href;
+    let content = "";
+    for(let i = 0 ; i < validateArr.length ; i++) {
+        if(i <= limit) {
+            content += `<a href="${validateArr[i].href}${currentUrl}" target="_blank">
+            <img src="${validateArr[i].img}" alt="${validateArr[i].alt}"></a>"<br/>`;
+        }
+    }
+    return content;
+}
